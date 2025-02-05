dfe28bb2d5cb8528eb1c0239fc229f237ce2b2f4
Neeki Askari <neeki.askari@gmail.com>
2025-02-05 14:49:58 -0800

    center on desktop view

--- src/components/pages/homePageStyled.tsx
+++ src/components/pages/homePageStyled.tsx
@@ -4,6 +4,7 @@ import { Button } from '@mui/material';
 
 export const HomePageOuterContainer = styled.div`
   width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
