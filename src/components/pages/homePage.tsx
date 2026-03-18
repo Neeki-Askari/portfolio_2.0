e89b211fbdde6029acdfacd0f9aa4db59f0c4ec4
Neeki Askari <neeki.askari@gmail.com>
2026-03-18 11:55:36 -0700

    project and resume updates

--- src/components/pages/homePage.tsx
+++ src/components/pages/homePage.tsx
@@ -1,54 +1,74 @@
-import { ButtonStyled, Header, HeaderContainer, HomePageOuterContainer, IntroContainer, LinksContainer, Name, PhotoContainer, SubHeader } from "./homePageStyled";
-import DownloadIcon from '@mui/icons-material/Download';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import {
+  ButtonStyled,
+  Header,
+  HeaderContainer,
+  HomePageOuterContainer,
+  IntroContainer,
+  LinksContainer,
+  Name,
+  PhotoContainer,
+  SubHeader,
+} from "./homePageStyled";
+import DownloadIcon from "@mui/icons-material/Download";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { ThemeContext, ThemeContextValues } from "../../context/themeContext";
 import { useContext } from "react";
 import { useOnIntersect } from "../../Intersect";
 
 const HomePage: React.FC = () => {
-    const {theme} = useContext(ThemeContext) as ThemeContextValues;
-    const [hiddenRef, isIntersecting] = useOnIntersect();
+  const { theme } = useContext(ThemeContext) as ThemeContextValues;
+  const [hiddenRef, isIntersecting] = useOnIntersect();
 
-    return (
-        <HomePageOuterContainer ref={hiddenRef} className={`${"home"} ${isIntersecting ? "fadeIn" : "hidden"}`}>
-            <PhotoContainer src="cropped.png"/>
-            <IntroContainer theme={theme}>
-                <HeaderContainer>
-                    <Header>Hello, my name is </Header>
-                    <Name>Neeki Askari</Name>
-                </HeaderContainer>
-                <SubHeader>I am a Full Stack Software Engineer eager to solve problems with innovative and efficient solutions.</SubHeader>
-                <LinksContainer className={theme}>
-                <ButtonStyled 
-                    theme={theme}
-                    startIcon={<DownloadIcon />}
-                    onClick={() => window.open("resume.pdf", "_blank")}
-                >
-                    Download my resume
-                </ButtonStyled>
-                <ButtonStyled
-                    theme={theme}
-                    startIcon={<LinkedInIcon fontSize="large"/>}
-                    onClick={() => window.open("https://www.linkedin.com/in/neeki-askari/", "_blank")}
-                >
-                    Connect on LinkedIn
-                </ButtonStyled>
-                </LinksContainer>
-                <ButtonStyled
-                theme={theme}
-                startIcon={<GitHubIcon fontSize="large"/>}
-                onClick={() => window.open("https://github.com/Neeki-Askari", "_blank")}
-                sx={{
-                    "marginTop": "8px",
-                    "width": "220px"
-                }}
-                >
-                    Check out my GitHub
-                </ButtonStyled>
-            </IntroContainer>
-        </HomePageOuterContainer>
-    )
-}
+  return (
+    <HomePageOuterContainer
+      ref={hiddenRef}
+      className={`${"home"} ${isIntersecting ? "fadeIn" : "hidden"}`}
+    >
+      <PhotoContainer src="profile.jpeg" />
+      <IntroContainer theme={theme}>
+        <HeaderContainer>
+          <Header>Hello, my name is </Header>
+          <Name>Neeki Askari</Name>
+        </HeaderContainer>
+        <SubHeader>
+          I am a Full Stack Software Engineer eager to solve problems with
+          innovative and efficient solutions.
+        </SubHeader>
+        <LinksContainer className={theme}>
+          <ButtonStyled
+            theme={theme}
+            startIcon={<DownloadIcon />}
+            onClick={() => window.open("resume.pdf", "_blank")}
+          >
+            Download my resume
+          </ButtonStyled>
+          <ButtonStyled
+            theme={theme}
+            startIcon={<LinkedInIcon fontSize="large" />}
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/neeki-askari/", "_blank")
+            }
+          >
+            Connect on LinkedIn
+          </ButtonStyled>
+        </LinksContainer>
+        <ButtonStyled
+          theme={theme}
+          startIcon={<GitHubIcon fontSize="large" />}
+          onClick={() =>
+            window.open("https://github.com/Neeki-Askari", "_blank")
+          }
+          sx={{
+            marginTop: "8px",
+            width: "220px",
+          }}
+        >
+          Check out my GitHub
+        </ButtonStyled>
+      </IntroContainer>
+    </HomePageOuterContainer>
+  );
+};
 
 export default HomePage;
--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -1,48 +1,59 @@
 import { useOnIntersect } from "../../../Intersect";
-import { ProjectCardType } from "../../../models/projectCardType"
-import ProjectCard from "./ProjectCard"
-import { Header, ProjectsInnerContainer, ProjectsOuterContainer, SubHeader } from "./ProjectsStyled"
+import { ProjectCardType } from "../../../models/projectCardType";
+import ProjectCard from "./ProjectCard";
+import {
+  Header,
+  ProjectsInnerContainer,
+  ProjectsOuterContainer,
+  SubHeader,
+} from "./ProjectsStyled";
 
 const Projects: React.FC = () => {
-    const [hiddenRef, isIntersecting] = useOnIntersect();
+  const [hiddenRef, isIntersecting] = useOnIntersect();
 
-    const projectInformation: ProjectCardType[] = [
-        {
-            projectName: "Med-Buddy",
-            image: "/med-buddy.png",
-            description: "Full stack web application equipped with OAuth, PostgreSQL, and ReactJS to allow users to easily manage their medications in one place.",
-            sourceCodeUrl: "https://github.com/Neeki-Askari/med-buddy"
-        },
-        {
-            projectName: "Portfolio",
-            image: "/portfolio-light.png",
-            description: "Front end portfolio website with mobile first design and deployed with AWS Cloud Architecture for proper Continuious Integration and Continuous Development (CI/CD).",
-            sourceCodeUrl: "https://github.com/Neeki-Askari/portfolio_2.0"
-        },
-        {
-            projectName: "HyperLoop API",
-            image: "/hyperloop-tech.png",
-            description: "Backend API for an e-commerce site that efficiently handles ETL (Extract, Transform, Load) on large datasets and optimizes SQL queries for latency.",
-            sourceCodeUrl: "https://github.com/Neeki-Askari/Hyperloop-API"
-        }
-    ]
-    return(
-        <ProjectsOuterContainer ref={hiddenRef} className={`projects ${isIntersecting ? "fadeIn" : "hidden"}`}>
-            <Header>Personal Projects</Header>
-            <SubHeader>Click on each project to view more details on GitHub</SubHeader>
-            <ProjectsInnerContainer>
-                {projectInformation.map((project, i) => {
-                    return (
-                        <div key={i}>
-                        <ProjectCard 
-                            project={project}
-                        />
-                        </div>
-                    )
-                })}
-            </ProjectsInnerContainer>
-        </ProjectsOuterContainer>
-    )
-}
+  const projectInformation: ProjectCardType[] = [
+    {
+      projectName: "SmartPlates",
+      image: "/SmartPlates.png",
+      description:
+        "Full stack application using Typescript, ReactJS, and ASP.NET to create and manage recipes, meal plans, and grocery lists.",
+      sourceCodeUrl: "https://github.com/Neeki-Askari/",
+    },
+    {
+      projectName: "Med-Buddy",
+      image: "/med-buddy.png",
+      description:
+        "Full stack web application equipped with OAuth, PostgreSQL, and ReactJS to allow users to easily manage their medications in one place.",
+      sourceCodeUrl: "https://github.com/Neeki-Askari/med-buddy",
+    },
+    {
+      projectName: "HyperLoop API",
+      image: "/hyperloop-tech.png",
+      description:
+        "Backend API for an e-commerce site that efficiently handles ETL (Extract, Transform, Load) on large datasets and optimizes SQL queries for latency.",
+      sourceCodeUrl: "https://github.com/Neeki-Askari/Hyperloop-API",
+    },
+  ];
+  return (
+    <ProjectsOuterContainer
+      ref={hiddenRef}
+      className={`projects ${isIntersecting ? "fadeIn" : "hidden"}`}
+    >
+      <Header>Personal Projects</Header>
+      <SubHeader>
+        Click on each project to view more details on GitHub
+      </SubHeader>
+      <ProjectsInnerContainer>
+        {projectInformation.map((project, i) => {
+          return (
+            <div key={i}>
+              <ProjectCard project={project} />
+            </div>
+          );
+        })}
+      </ProjectsInnerContainer>
+    </ProjectsOuterContainer>
+  );
+};
 
 export default Projects;
